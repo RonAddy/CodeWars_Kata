3a0ba597cf9b66cfa270a05c0ad2b97fbc40f45c
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-02-17 23:38:52 -0500

    Made some edits to comments

--- L8/String_to_number.js
+++ L8/String_to_number.js
@@ -8,7 +8,6 @@ var stringToNumber = function(str) {
 // stringToNumber("1234") == 1234
 // stringToNumber("605" ) == 605
 // stringToNumber("1405") == 1405
-// stringToNumber("-7"  ) == -7
 
 // Research these below
 // 1.
