c0a5efd935bfb0c85e9d3be0edde9ba8878f538f
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-03-06 03:00:43 -0500

    Progress with returning values in bookseller kata.Need to revisit function

--- L6/Help_The_Bookseller.js
+++ L6/Help_The_Bookseller.js
@@ -1,14 +1,18 @@
-function stockList(listOfArt, listOfCat){
-    if ( typeof listOfArt === Array  && typeof listOfCat === Array) {
-        console.log(true);
-        return true
-    }
-    else {
-        console.log(false);
-        return false
-    }
+function stockList(listOfArt, listOfCat) {
+  const bookResults = {}
+  if (Array.isArray(listOfArt) && Array.isArray(listOfCat)) {
+    // must find way to dynamically increment values when duplicate is found
+    listOfArt.forEach(ele => {
+        if(bookResults[ele[0]] === null){
+
+        }
+      bookResults[ele[0]] += parseInt(ele.substr(ele.search(/[0-9]/), ele[ele.length]))
+    })
+    console.log(bookResults)
+  } else {
+    console.log(false);
+    return false
+  }
 }
 
-
-
-stockList(["hey", 1], ["cat",1])
+stockList(["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"], ["A", "B"])
