7fd16061255c64a9f7000cd1402060eb947c7ad9
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-04-05 23:59:12 -0400

    add Mexican wave Kata

---
+++ L6/Mexican_Wave.js
@@ -0,0 +1,11 @@
+function wave(input){
+    let ans = [].map.call(input, (x, i) => {
+    let index = i
+    if(i = 0) return bigX + input.substr(i+1)
+    
+    input = input.toLowerCase()
+    return input.substr(0, index) + x.toUpperCase()+ input.substr(index+1)
+    })
+    if (!ans.forEach( x => match(/[A-Z/))
+    return ans
+  }
