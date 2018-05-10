5f83e62018dc49c51c8363cf91cef8f8e5a734b8
Aaron Bart-Addison <abartaddison12@gmail.com>
2018-05-10 15:42:25 -0400

    completed the Master of Files Kata

---
+++ L6/Master_Of_Files.js
@@ -0,0 +1,46 @@
+// Are you a file extension master? Let's find out by checking if Bill's files are images or audio files. Please use regex if available natively for your language.
+
+// You will create 2 string methods:
+
+// @isAudio/@is_audio:
+//  matching 1 or + uppercase/lowercase letter(s) (combination possible), with the extension .mp3, .flac, .alac, or .aac.
+
+// @isImage/@is_image:
+//  matching 1 or + uppercase/lowercase letter(s) (combination possible), with the extension .jpg, .jpeg, .png, .bmp, or .gif.
+
+// Note that this is not a generic image/audio files checker. It's meant to be a test for Bill's files only. Bill doesn't like punctuation. He doesn't like numbers, neither. Thus, his filenames are letter-only
+
+// Rules:
+
+// It should return true or false, simply.
+// File extensions should consist of lowercase letters and numbers only.
+// File names should consist of letters only (uppercase, lowercase, or both)
+function validateString(str, validExt) {
+    const ext = str.slice(str.lastIndexOf('.'))
+    const filename = str.slice(0, str.indexOf(ext))
+    let ans;
+
+    validExt.filter( x => ext === x ? ans = true : null)
+
+    if( filename.match(/\s/) || filename.match(/[^a-zA-Z]/g)){
+        ans = false
+    }
+    
+    return ans || false
+}
+
+String.prototype.isAudio= function(){
+    const extensions = ['.mp3', '.flac', '.alac', '.aac']
+    return validateString(this, extensions)
+  };
+
+  String.prototype.isImage= function(){
+    const extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif']
+    return validateString(this, extensions)
+  };
+
+
+  "DaftPunk.FLAC".isAudio()
+  "NothingElseMatters.mp3".isAudio()
+  "Nothing Else Matters.mp3".isAudio()
+  " Amon Tobin.alac".isAudio()
---
+++ L6/Street_Fighter_2_Character_Selection.js
@@ -0,0 +1,27 @@
+const fighters = [
+	["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+	["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+];
+
+function streetFighterSelection(fighters, position, moves) {
+  const posX = position[0]
+  const posY = position[1]
+  const indx = 0
+  const curr = fighters[indx]
+  const ans = []  
+
+  moves.forEach(ele => {
+        ele === 'up' ? posY -= 1 : null
+        ele === 'down' ? posY += 1 : null
+        ele === 'left' ? posX -= 1 : null
+        ele === 'right' ? posX += 1 : null
+
+        if ((posX === 5 || posY === 5) && ele === "right") {
+          curr = 0
+          ans.push(fighters[curr])
+        } else if ((posX === 0 || posY === 0) && moves[indx] === "left") {
+          curr = 5
+          ans.push(fighters[curr])
+        }
+
+      }
